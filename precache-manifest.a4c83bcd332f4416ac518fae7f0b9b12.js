830ac64f43e819e37dc689d5bef1246388c0c2aa
Harry Chiu <harrychiu1106@gmail.com>
2020-04-08 18:17:02 +0800

    Updates

---
+++ precache-manifest.a4c83bcd332f4416ac518fae7f0b9b12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01af6691701032447a46499c9c1e7aa6",
+    "url": "/Bin2Dec/index.html"
+  },
+  {
+    "revision": "55ce4c26e6458cb27aa8",
+    "url": "/Bin2Dec/static/css/main.1230bb88.chunk.css"
+  },
+  {
+    "revision": "7541dfdfbfa294e5b732",
+    "url": "/Bin2Dec/static/js/2.2874e51f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Bin2Dec/static/js/2.2874e51f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55ce4c26e6458cb27aa8",
+    "url": "/Bin2Dec/static/js/main.6f77d6bf.chunk.js"
+  },
+  {
+    "revision": "8227d3dbc4d69f302fb1",
+    "url": "/Bin2Dec/static/js/runtime-main.bb8bce80.js"
+  }
+]);
